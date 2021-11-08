4fc8a850bfa1959248ed0562f74e2d546ea1add8
Alexandra <aleksandra.alexand2017@yandex.ru>
2021-11-08 22:40:50 +0300

    clear

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,5 +1,4 @@
 """YaMDb URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
@@ -13,9 +12,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# test commit
+
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
