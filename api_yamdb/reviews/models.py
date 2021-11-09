28897d221a99dc862ddd1f00dea62bb1b5e6d07d
Dmitry Tok <moon0939110824@gmail.com>
2021-11-09 20:09:21 +0200

    create models Genre, Cetegory, Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,1 +1,70 @@
 from django.db import models
+
+
+class Genre(models.Model):
+    """
+    Жанры произведений.
+    Одно произведение может быть привязано к нескольким жанрам.
+    """
+    name = models.CharField(max_length=200, verbose_name='Жанр', unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    """
+    Категории (типы) произведений («Фильмы», «Книги», «Музыка»).
+    """
+    name = models.CharField(max_length=200, verbose_name='Категория', unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    """
+    Произведения, к которым пишут отзывы
+    (определённый фильм, книга или композиция).
+    """
+    name = models.CharField(max_length=200, verbose_name='Произведение')
+    year = models.IntegerField(
+        verbose_name='Год издания',
+        blank=True,
+        null=True,
+    )
+    description = models.CharField(
+        max_length=200,
+        verbouse_name='Описание',
+        blank=True,
+        null=True,
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
