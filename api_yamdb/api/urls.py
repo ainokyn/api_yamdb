6fd48f5ab8c43313bf1577049ef00adc1d786e8a
Dmitry Tok <moon0939110824@gmail.com>
2021-11-10 22:34:50 +0200

    add viewsets, routers, serializers for Category, Genre

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 
 from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
 
-router_v1 = routers.DefaultRouter()
-router_v1.register('categories', CategoryViewSet, basename='category')
-router_v1.register('genres', GenreViewSet, basename='genre')
-router_v1.register('titles', TitleViewSet, basename='title')
+router = DefaultRouter()
+router.register('categories', CategoryViewSet, basename='category')
+router.register('genres', GenreViewSet, basename='genre')
+router.register('titles', TitleViewSet, basename='title')
 
 urlpatterns = [
-    path('v1/', include(router_v1.urls)),
+    path('v1/', include(router.urls)),
 ]
