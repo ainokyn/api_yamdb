124ba236390462caf8c95419f40666791d8322a4
Alexandra <aleksandra.alexand2017@yandex.ru>
2021-11-16 00:44:58 +0300

    pep8

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-
-from .validate import validate_year
 from reviews.models import Category, Comments, Genre, GenreTitle, Review, Title
 
+from .validate import validate_year
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
