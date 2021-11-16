3198640f5442db1c50f79a213b429250daef87e6
Alexandra <aleksandra.alexand2017@yandex.ru>
2021-11-16 23:36:48 +0300

    pep8

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -13,7 +13,6 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from reviews.models import Category, Comments, Genre, Review, Title
 
 from .customfilters import TitlesFilter
