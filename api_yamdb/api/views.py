a209f7d7b5336e7095ed1f4f24b65dd610c02726
Alexandra <aleksandra.alexand2017@yandex.ru>
2021-11-16 03:12:35 +0300

    pep8

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -8,10 +8,10 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db.models import Avg
 from reviews.models import Category, Comments, Genre, Review, Title
 
