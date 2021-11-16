ff6cace8abbc4ec7d4fb5f813a400d64ab592841
Alexandra <aleksandra.alexand2017@yandex.ru>
2021-11-16 03:46:17 +0300

    pep8

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -8,7 +8,6 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
@@ -16,12 +15,12 @@ from django.core.mail import send_mail
 from django.db.models import Avg
 
 from reviews.models import Category, Comments, Genre, Review, Title
-from .customfilters import TitlesFilter
 from .permissions import AnonymModeratorAdminAuthor, IsAdmin, IsAdminOrReadOnly
 from .serializers import (CategorySerializer, CommentsSerializer,
                           GenreSerializer, ReviewSerializer, SignUpSerializer,
                           TitleReadSerializer, TitleWriteSerializer,
                           TokenRequestSerializer, UserSerializer)
+from .customfilters import TitlesFilter
 
 User = get_user_model()
 
