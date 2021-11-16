bfa3f68c3dcf1e2902695f7a291581e00ce8bb9a
Alexandra <aleksandra.alexand2017@yandex.ru>
2021-11-16 03:04:40 +0300

    pep8

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -13,7 +13,6 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.db.models import Avg
-
 from reviews.models import Category, Comments, Genre, Review, Title
 
 from .customfilters import TitlesFilter
