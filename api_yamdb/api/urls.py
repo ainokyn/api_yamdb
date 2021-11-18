c75f7837a6ab97fb7c28273cc0b75269b511172e
Alexandra <aleksandra.alexand2017@yandex.ru>
2021-11-19 00:23:01 +0300

    pep8

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (CategoryViewSet, CommentsViewSet, GenreViewSet,
                     ReviewViewSet, TitleViewSet, UserViewSet)
-
 from .views import create_token, sign_up
 
 router = DefaultRouter()
