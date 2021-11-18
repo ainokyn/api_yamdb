0e2779757be05c28f3809b6a2c0bf1f67d254fdb
Alexandra <aleksandra.alexand2017@yandex.ru>
2021-11-19 00:18:00 +0300

    pep8

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,7 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import (create_token, sign_up, CategoryViewSet, CommentsViewSet,
                     GenreViewSet, ReviewViewSet, TitleViewSet,
                     UserViewSet)
+
 router = DefaultRouter()
+
 router.register(r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet,
                 basename='reviews')
 router.register(
