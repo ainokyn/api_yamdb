84d939b247acfc6d7d65b92cad75be0576d3b015
Alexandra <aleksandra.alexand2017@yandex.ru>
2021-11-18 23:53:14 +0300

    pep8

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
+from .views import create_token, sign_up
 from .views import (CategoryViewSet, CommentsViewSet,
                     GenreViewSet, ReviewViewSet, TitleViewSet,
                     UserViewSet)
-from .views import create_token, sign_up
 
 router = DefaultRouter()
 router.register(r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet,
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -7,10 +7,9 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import (filters, mixins, pagination, permissions, status,
                             viewsets)
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework_simplejwt.tokens import RefreshToken
 from reviews.models import Category, Genre, Review, Title
 
